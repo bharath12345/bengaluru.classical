f6ad8c21fd035725c865fbb5c02b872b285390ff
Bharadwaj <bharath12345@gmail.com>
2026-07-20 11:05:26 +0530

    style: format prod settings for ruff CI check

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,9 +7,7 @@ from .base import BASE_DIR, MIDDLEWARE
 
 DEBUG = False
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS", "bengaluruclassical.in,*.run.app"
-).split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "bengaluruclassical.in,*.run.app").split(",")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
